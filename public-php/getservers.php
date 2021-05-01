a9814fe35c1fe2e1943f8e07bc6f8cc5045dfad8
quantumr8 <ross.samuel.17@gmail.com>
2021-05-01 16:13:33 -0500

    update url

--- public-php/getservers.php
+++ public-php/getservers.php
@@ -3,6 +3,6 @@ $username = $argv[1];
 $apikey = $argv[2];
 $id = $argv[3];
 require('MulticraftAPI.php');
-$api = new MulticraftAPI('https://mc.pebblehost.com/api.php', $username, $apikey);
+$api = new MulticraftAPI('https://panel.pebblehost.com/api.php', $username, $apikey);
 print_r($api->listServersByOwner($id));
 ?>
--- public-php/verifydetails.php
+++ public-php/verifydetails.php
@@ -2,6 +2,6 @@
 $username = $argv[1];
 $apikey = $argv[2];
 require('MulticraftAPI.php');
-$api = new MulticraftAPI('https://mc.pebblehost.com/api.php', $username, $apikey);
+$api = new MulticraftAPI('https://panel.pebblehost.com/api.php', $username, $apikey);
 print_r($api->getCurrentUser());
 ?>
