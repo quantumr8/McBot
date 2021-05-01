529a4bf7ea04bc9df0668a06c947d86a604d9bb8
quantumr8 <ross.samuel.17@gmail.com>
2021-05-01 16:24:22 -0500

    startover #2

--- public-php/getserverstatus.php
+++
@@ -1,8 +0,0 @@
-<?php
-$username = $argv[1];
-$apikey = $argv[2];
-$id = $argv[3];
-require('MulticraftAPI.php');
-$api = new MulticraftAPI('https://panel.pebblehost.com/api.php', $username, $apikey);
-print_r($api->getServerStatus($id));
-?>
